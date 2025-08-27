31bce29fb36f853d5fc6519a8db38067d15c643c
HyunJu Lee <lhj7826@naver.com>
2025-08-27 15:32:39 +0900

    feat: user model 생성

---
+++ app/user/model.py
@@ -0,0 +1,30 @@
+from enum import StrEnum
+
+from tortoise import fields
+from tortoise.models import Model
+
+
+class UserRole(StrEnum):
+    USER = "user"
+    STAFF = "staff"
+    SUPERUSER = "superuser"
+
+
+class User(Model):
+    id = fields.BigIntField(pk=True, generated=True)  # 사용자 ID, AUTO_INCREMENT
+    nickname = fields.CharField(max_length=20, unique=True)  # 로그인 ID
+    email = fields.CharField(max_length=100, unique=True)  # 이메일
+    password = fields.CharField(max_length=255)  # 패스워드
+    username = fields.CharField(max_length=20)  # 이름
+    phonenumber = fields.CharField(max_length=20)  # 연락처
+    lastlogin = fields.DatetimeField(null=True)  # 마지막 로그인
+    account_activation = fields.BooleanField(default=False)  # 계정 활성화 여부
+    created_at = fields.DatetimeField(auto_now_add=True)  # 생성일
+    updated_at = fields.DatetimeField(auto_now=True)  # 수정일
+    user_roles = fields.CharEnumField(
+        enum_type=UserRole, default=UserRole.USER
+    )  # 유저 권한
+
+    class Meta:
+        table = "users"
+        ordering = ["-created_at"]
