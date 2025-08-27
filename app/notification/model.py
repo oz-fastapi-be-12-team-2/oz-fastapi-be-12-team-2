7982743ae6aff1a26c3e6b99cfe1a035a86cb52f
lsh9511 <iamshopsh@naver.com>
2025-08-27 16:11:21 +0900

    ✨ feat : 새로운 기능 추가 감정알림 모델, 타임스탬프믹스인 모델 추가

--- app/notification/model.py
+++ app/notification/model.py
@@ -5,17 +5,18 @@ from tortoise.models import Model
 
 from app.shared.models import TimestampMixin
 
+
 class AlertType(StrEnum):
     PUSH = "PUSH"
     EMAIL = "EMAIL"
     SMS = "SMS"
 
 
-
-class Notification(Model,TimestampMixin):
+class Notification(Model, TimestampMixin):
     alert_id = fields.BigIntField(pk=True)
     content = fields.CharField(max_length=255)
-    alert_type =fields.CharEnumField(AlertType)
+    alert_type = fields.CharEnumField(AlertType)
 
-class Meta :
+
+class Meta:
     table = "감정 알림"
