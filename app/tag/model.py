83cdbd00992bb6837c725aedbf2ed4d76028764d
SeongSu <kimsszzz4@naver.com>
2025-08-27 16:51:44 +0900

    ✨ Tag model.py 생성
    Tag model.py 내용 작성

---
+++ app/tag/model.py
@@ -0,0 +1,15 @@
+from tortoise import fields
+from tortoise.models import Model
+
+class Tag(Model):
+    tag_id = fields.BigIntField(pk=True, generated=True)  # 태그 ID, AUTO_INCREMENT
+    tag_name = fields.CharField(max_length=50, unique=True)
+
+    diaries: fields.ManyToManyRelation["Diary"] = fields.ManyToManyField(
+        "models.Diary",  # Diary 모델 참조
+        related_name="tags",  # Diary에서 역참조 이름
+        through="diary_tags"  # 중간 테이블 이름
+    )
+
+    class Meta:
+        table = "tags"
