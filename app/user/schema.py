af8c0af24652c0e018fdf484d8f3613db3c262c1
SeongSu <kimsszzz4@naver.com>
2025-08-29 17:23:26 +0900

    ✨ feat : user 잔여오류 수정 및 API.py 작성

--- app/user/schema.py
+++ app/user/schema.py
@@ -1,21 +1,24 @@
 from datetime import datetime
+from typing import Annotated
 
-from pydantic import BaseModel, EmailStr, Field, constr
+from pydantic import BaseModel, EmailStr, Field
 
 
 # 회원가입 시 요청 스키마
 class UserCreate(BaseModel):
     email: EmailStr  # 이메일 형식 자동 검증
-    password: constr(min_length=8)  # 최소 8자 이상
-    nickname: constr(min_length=2, max_length=20)
+    password: Annotated[str, Field(min_length=8)]  # 최소 8자 이상
+    nickname: Annotated[str, Field(min_length=2, max_length=20)]
     username: str
-    phonenumber: str = Field(..., pattern=r"^010-\d{4}-\d{4}$")  # 010-0000-0000 형식
+    phonenumber: Annotated[
+        str, Field(pattern=r"^010-\d{4}-\d{4}$")
+    ]  # 010-0000-0000 형식
 
 
 # 로그인 요청 시 스키마
 class UserLogin(BaseModel):
     email: EmailStr
-    password: constr(min_length=8)  # UserCreate 부분과 통일했습니다.
+    password: Annotated[str, Field(min_length=8)]  # UserCreate와 통일
 
 
 # 응답용 (비밀번호 제외)
@@ -42,7 +45,3 @@ class Token(BaseModel):
 # 로그아웃 응답
 class LogoutResponse(BaseModel):
     message: str = "Logged out successfully"
-
-
-# class UserNotificationSettingUpdate(BaseModel):
-#    receive_notifications: bool = Field(..., description="알림 수신 여부")
