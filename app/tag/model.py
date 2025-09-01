e4ea116d19601b912caee6d969c05de0834513f6
HyunJu Lee <lhj7826@naver.com>
2025-09-01 17:39:01 +0900

    fix: tag 불필요한 참조 삭제

--- app/tag/model.py
+++ app/tag/model.py
@@ -1,18 +1,12 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from tortoise import fields
 from tortoise.models import Model
-
-if TYPE_CHECKING:
-    from app.diary.model import Diary  # 실제 Tag 모델이 정의된 경로에 맞춰 수정
 
 
 class Tag(Model):
     id = fields.IntField(pk=True, generated=True)
     name = fields.CharField(max_length=50, unique=True)
-    diaries: fields.ManyToManyRelation["Diary"]
 
     class Meta:
         table = "tags"
