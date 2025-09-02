5a2ba11d2a3905241526ebc04e58fa4aadb05867
lsh9511 <iamshopsh@naver.com>
2025-09-02 11:03:14 +0900

    🐛 fix : 기능 수정, 버그 수정 tag 기능 최종

--- app/main.py
+++ app/main.py
@@ -119,8 +119,8 @@ app.openapi = custom_openapi
 # ─────────────────────────────────────────────────────────────
 # 라우터 등록
 # ─────────────────────────────────────────────────────────────
-app.include_router(ai_router)
 app.include_router(tag_router)
+app.include_router(ai_router)
 app.include_router(user_router)
 app.include_router(diary_router)
 app.include_router(notification_router)
